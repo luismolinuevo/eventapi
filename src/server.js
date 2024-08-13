44a73d1518bbadd6a535bc9b16928974b4f296e6
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-08-12 21:01:36 -0400

    added rate limiter

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import limiter from "./config/rateLimiter.js";
 import apiRouter from "./routes/index.js";
 import globalErrorHandler from "./middlewares/globalError.js";
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(cors());
+app.use(limiter);
 
 app.use("/api", apiRouter);
 app.use(globalErrorHandler);
