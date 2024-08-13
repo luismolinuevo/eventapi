ef030537acbbefc6325714c1280347e021963125
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-08-12 20:52:49 -0400

    installed helmet and cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 import apiRouter from "./routes/index.js";
 import globalErrorHandler from "./middlewares/globalError.js";
 
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(morgan("tiny"));
+app.use(helmet());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.use(globalErrorHandler);
